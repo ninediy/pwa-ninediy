086c76d385f29d88f86e80f5139be5fccac8b2f9
naruebaet bouhom <naruebaet.b@garena.co.th>
2017-06-25 18:43:41 +0700

    update theme
    change theme to black pink hahahahaha

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FootballService } from '../services/football.service';
 import { NoteFormComponent } from '../dialog/note-form/note-form.component';
-import { MdDialog } from '@angular/material';
+import { MdDialog, MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-note',
-  templateUrl: './note.component.html',  
+  templateUrl: './note.component.html',
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
@@ -14,11 +14,16 @@ export class NoteComponent implements OnInit {
   public data = {
     txtNote: ''
   }
-  constructor(private footballService: FootballService, private dialog: MdDialog) {
+  constructor(
+    private footballService: FootballService,
+    private dialog: MdDialog,
+    private snackBar: MdSnackBar
+  ) {
   }
 
   ngOnInit(): void {
     this.doGetMyNote();
+    this.openSnackBar('คุณสามารถใช้ Note ในโหมด offline ได้','tips');
   }
 
   doGetMyNote(): void {
@@ -41,6 +46,12 @@ export class NoteComponent implements OnInit {
 
   closeDialog() {
     this.dialog.closeAll();
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
 }
